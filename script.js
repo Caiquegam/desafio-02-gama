edd10dbee884bd6ccfac7efc22dad1faa92627fc
Caiquegam <caiquegamadeoliveira@gmail.com>
2022-11-15 18:54:35 -0300

    Atualização da data no script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ async function getListarEventos() {
     const cardEventos = `
     <div class="conjuntoCards">
         <article class="evento card p-5 m-3">
-            <h2>${lista.name} - 05/03/2022</h2>
+            <h2>${lista.name} - ${lista.scheduled}</h2>
             <h4>${lista.attractions}</h4>
             <p>${lista.description}</p>
-            <a href="#" class="btn btn-primary">reservar ingresso</a>
+            <a href="#" class="btn btn-primary">Reservar Ingresso</a>
         </article>
     </div>
   `;
@@ -28,7 +28,7 @@ if (window.location.pathname.includes("eventos")) {
 
 async function cadastrarEvento(event) {
   event.preventDefault();
-  
+
   const nome = document.querySelector("#nome").value;
   const link = document.querySelector("#link").value;
   const atracao = document.querySelector("#atracoes").value.split(",");
@@ -41,7 +41,7 @@ async function cadastrarEvento(event) {
     poster: link,
     attractions: atracao,
     description: descricao,
-    scheduled: new Date (data).toISOString(),
+    scheduled: new Date(data).toISOString(),
     number_tickets: lotacao,
   };
 
@@ -53,17 +53,16 @@ async function cadastrarEvento(event) {
     body: JSON.stringify(dataObjeto),
   });
 
-  limparFormulario("#nome") 
-  limparFormulario("#link") 
-  limparFormulario("#atracoes") 
-  limparFormulario("#descricao") 
-  limparFormulario("#data") 
-  limparFormulario("#lotacao") 
-    
-} 
+  limparFormulario("#nome");
+  limparFormulario("#link");
+  limparFormulario("#atracoes");
+  limparFormulario("#descricao");
+  limparFormulario("#data");
+  limparFormulario("#lotacao");
+}
 
-function limparFormulario(id){
-    const elemento = document.querySelector(id)
+function limparFormulario(id) {
+  const elemento = document.querySelector(id);
 
-    elemento.value = "";
+  elemento.value = "";
 }
